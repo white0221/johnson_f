f22be8011874162c3b25d420b402c9227ab1a7e1
white0221 <r_tsuchiya@ga-tech.co.jp>
2020-09-21 17:07:27 +0900

    add home about page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Navbar />
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+        </Router>
       </header>
     </div>
   );
